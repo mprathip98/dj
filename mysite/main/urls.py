dfc41fe82f900b806211aff5b67168106219f248
Mohit Prathipati <prathipati.mohit@gmail.com>
2025-07-22 14:46:59 -0700

    initial

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("<int:id>", views.index, name="index"),
-    path("", views.index, name="index"),
+    path("", views.home, name="home"),
+    path("create/", views.create, name="create"),
+
  ]
